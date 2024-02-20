3dd634f1884dbf481bf07fc3382f72470f960e58
Daniel_Carreño <90060617+4NG3LD4NI3L@users.noreply.github.com>
2024-02-19 22:42:06 -0700

    Delete src directory

--- src/button.js
+++
@@ -1,8 +0,0 @@
-function button(texto, desactivado) {
-    var boton = document.createElement('button');
-    boton.textContent = texto;
-    boton.disabled = desactivado;
-    return boton;
-  }
-
-  export default button;

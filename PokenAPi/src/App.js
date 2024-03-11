aaf0337c025ebb3e418ea1bb760bbb468d217b3b
Daniel_Carreño <90060617+4NG3LD4NI3L@users.noreply.github.com>
2024-03-10 22:56:21 -0700

    Add files via upload

--- PokenAPi/src/App.js
+++ PokenAPi/src/App.js
@@ -4,57 +4,71 @@ import { getPokemonById } from './components/pokemonApi';
 import './App.css';
 import Search from './components/buscar';
 
-
 function App() {
-
   const [pokemonData, setPokemonData] = useState([]);
+  const [resultados, setResultados] = useState(pokemonData);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
-      try{// Obtener información de varios Pokémon
-        const pokemonIds = Array.from({length:50}, (_,index) => index + 1);
+      try {
+        const pokemonIds = Array.from({ length: 50 }, (_, index) => index + 1);
         const pokemonPromises = pokemonIds.map(id => getPokemonById(id));
         const pokemonInfo = await Promise.all(pokemonPromises);
         setPokemonData(pokemonInfo);
-      }catch(error){
-        console.error('ERRO pokemon data:', error);
+      } catch (error) {
+        console.error('Error fetching Pokemon data:', error);
       }
-
     };
 
     fetchPokemonData();
   }, []);
 
+  const handleSearch = (query) => {
+    // Filtrar los resultados según la consulta de búsqueda
+    const filteredResults = pokemonData.filter(pokemon => 
+      pokemon.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setResultados(filteredResults);
+  };
+
+  
+
   return (
     <div className="App">
-
       <div className='Encabezado'>
-        <img src='https://cdn.pixabay.com/photo/2016/07/13/08/31/pokemon-1513925_1280.jpg' width={200} height={200}></img>
+        <img src='https://cdn.pixabay.com/photo/2016/07/13/08/31/pokemon-1513925_1280.jpg' width={200} height={200} alt="Pokemon" />
         <h1>PokemonApi</h1>
       </div>
 
-     
       <div className='buscar-container'>
-      <Search></Search>
+        <Search  onSearch={handleSearch} className="buscador"/>
       </div>
 
-      <div className="App">
       <div className='product-list'>
-        {pokemonData.map(pokemon => (
-          <ProductCard
-            key={pokemon.id}
-            image={pokemon.sprites.front_default}
-            name={pokemon.name}
-            description={pokemon.types.map(type => type.type.name).join(', ')}
-            fullDescription={pokemon.species.name}
-          />
-        ))}
-      </div>
-    </div>
-        
+    {resultados.length > 0 ? (
+        resultados.map(pokemon => (
+            <ProductCard
+                key={pokemon.id}
+                image={pokemon.sprites.front_default}
+                name={pokemon.name}
+                description={pokemon.types.map(type => type.type.name).join(', ')}
+                fullDescription={`Abilities: ${pokemon.abilities.map(ability => ability.ability.name).join(' <--> ')}, Moves: ${pokemon.moves.map(move => move.move.name).join(' <-->')}`}
+            />
+        ))
+    ) : (
+        pokemonData.map(pokemon => (
+            <ProductCard
+                key={pokemon.id}
+                image={pokemon.sprites.front_default}
+                name={pokemon.name}
+                description={pokemon.types.map(type => type.type.name).join(', ')}
+                fullDescription={`Abilities: ${pokemon.abilities.map(ability => ability.ability.name).join(' <--> ')}, Moves: ${pokemon.moves.map(move => move.move.name).join(' <-->')}`}
+            />
+        ))
+    )}
+</div>
     </div>
   );
-
 }
 
 export default App;
